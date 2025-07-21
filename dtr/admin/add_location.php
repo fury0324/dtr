f5e580170894eca3b40aa6483669df0e8fed3d54
fury0324 <antoniosteven664@gmail.com>
2025-07-21 17:50:36 +0800

    Delete dtr directory

--- dtr/admin/add_location.php
+++
@@ -1,7 +0,0 @@
-  <form method="POST" action="process_location.php">
-    Name: <input name="name" required><br>
-    Latitude: <input name="lat" required><br>
-    Longitude: <input name="lng" required><br>
-    Radius (meters): <input name="radius" required><br>
-    <button>Save</button>
-  </form>
